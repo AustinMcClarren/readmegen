5f96321b4459f0ec14ad7b399926e84298a70f0d
Austin Mcclarren <austinjmccl123@icloud.com>
2023-01-16 20:29:27 -0500

    added global requires and made an array of questions

---
+++ Develop/index.js
@@ -0,0 +1,105 @@
+//all global requires 
+import inquirer from ("inquirer");
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+console.log('Hello, welcome to my readme gen!')
+console.log('Answer the following questions!')
+
+
+
+  // TODO: Create an array of questions for user input
+const questions = () => {
+	return inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "What is the title of your project?",
+		},
+		{
+			type: "input",
+			name: "description",
+			message: "What is your project about?",
+		},
+		{
+			type: "input",
+			name: "installation",
+			message: "How did you install your project?",
+		},
+		{
+			type: "input",
+			name: "usage",
+			message: "how do you use your project?",
+		},
+		{
+			type: "checkbox",
+			name: "license",
+			message: "Choose a license!",
+			choices: ['MIT', 'MPL', 'GPL', 'BSD', 'NONE']
+		},
+		{
+			type: "input",
+			name: "contact",
+			message: "how can they contact you?",
+		},
+			{
+			type: "input",
+			name: "GitHub",
+			message: "Enter your GitHub Username?",
+		},
+	]);
+};
+
+
+
+
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName,data, (err) =>{
+		if (err){
+			return console.log(err);
+		}
+
+		console.log('Congrats! you can now look at your Readme!')
+	})
+
+}
+
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+	inquirer.prompt(questions)
+	.then
+}
+
+
+
+
+
+
+
+
+// Function call to initialize app
+init();
+
+
+
+
+// GIVEN a command-line application that accepts user input
+// WHEN I am prompted for information about my application repository
+// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
